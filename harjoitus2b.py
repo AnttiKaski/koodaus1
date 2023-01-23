2edb5aaaee5136fbee5f09c2e424a4456f8c5ef6
Antti Kaski <122267595+AnttiKaski@users.noreply.github.com>
2023-01-23 14:50:54 +0200

    muutos

--- harjoitus2b.py
+++ harjoitus2b.py
@@ -2,6 +2,11 @@ import Harjoitus2 as H2
 from Harjoitus2 import kysy_nimet
 
 if __name__== "__main__":
+    joku_juttu = 1
+
     print("Kysytään 2 nimeä:")
-    nimet = Harjoitus2.kysy_nimet(2)
+    nimet = H2.kysy_nimet(2)
+    joku_juttu = 2
+    nimet.append("Ekstra nimi")
+    joku_juttu = 3
     print ("Nimet ovat", nimet)
